00b8d084a208ef4c3d2a5e362c6f9f4c8097a3ed
aliveevie <aliveibi080@gmail.com>
2024-03-01 10:57:22 +0100

    Update files

--- contract/src/contract.ts
+++ contract/src/contract.ts
@@ -3,21 +3,5 @@ import { NearBindgen, near, call, view } from 'near-sdk-js';
   // This editor accept only Typescript
   // Dont Worry about the import error
   // Just build your contract
-
-  @NearBindgen({})
-  class Hello{
-      greeting:string = "Hello";
-
-      @view({})
-      get_greeting():string{
-        return this.greeting
-      }
-
-      @call({})
-      set_greeting({ message } : { message }):void{
-          this.greeting = message;
-          near.log(`You saved a message in the near blockchain ${message}`)
-      }
-  }
   
   
